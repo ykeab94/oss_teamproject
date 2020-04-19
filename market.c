f6c6a2cd309f3159b2a47177e3e3ea4877bdb8ce
Jee sung <21500706@handong.edu>
2020-04-19 16:08:10 +0900

    when selecting menu, if you select any number except menu number(1~8 or 0), print Error message and Reselect correct number

--- market.c
+++ market.c
@@ -6,18 +6,22 @@
 
 int selectMenu() {
     int menu;
-    printf("\n*** 과자 품목 관리 ***\n");
-    printf("1. 조회\n");
-    printf("2. 추가\n");
-    printf("3. 수정\n");
-    printf("4. 삭제\n");
-    printf("5. 파일저장\n");
-    printf("6. 이름검색\n");
-    printf("7. 가격검색\n");
-    printf("8. 별점검색\n");
-    printf("0. 종료\n");
-    printf("\n=> 원하는 메뉴는? ");
-    scanf("%d", &menu);
+    while(1) {
+    	printf("\n*** 과자 품목 관리 ***\n");
+    	printf("1. 조회\n");
+    	printf("2. 추가\n");
+    	printf("3. 수정\n");
+    	printf("4. 삭제\n");
+    	printf("5. 파일저장\n");
+    	printf("6. 이름검색\n");
+    	printf("7. 가격검색\n");
+    	printf("8. 별점검색\n");
+    	printf("0. 종료\n");
+    	printf("\n=> 원하는 메뉴는? ");
+    	scanf("%d", &menu);
+	// 아직 수정전
+	if() break;
+    }
     return menu;
 }
 
