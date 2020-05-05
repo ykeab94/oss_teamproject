63ba3aeae4066e950f54ff0d0178b523d70de678
Bae kiwan <21900335@handong.edu>
2020-05-06 00:28:42 +0900

    Added selectDataNo' head

--- project/manager.h
+++ project/manager.h
@@ -24,3 +24,6 @@ int deleteScore(Student *s);
 // 제품 리스트를 읽어오는 함수
 void listScore(Student *s, int count);
 
+// update와 delete 할 때, 번호을 물어보는 함수(updateProd, deleteProd, searchProd함수에서 사용)
+int selectDataNo(Student *s, int count);
+
